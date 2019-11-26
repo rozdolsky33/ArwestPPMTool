56243dc09f097b000dacfbd9ab45aa350eb51d71
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2019-11-25 23:24:27 -0500

    first commit

---
+++ PPMToolFullstack/src/main/java/com/arwest/app/developer/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.arwest.app.developer.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolApplication.class, args);
+    }
+
+}
