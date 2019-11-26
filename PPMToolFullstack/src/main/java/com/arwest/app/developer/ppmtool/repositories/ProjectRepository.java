c56f2c6db4782bd3916e987fbae26d83e4aecb66
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2019-11-26 00:11:09 -0500

    project entity - repository

---
+++ PPMToolFullstack/src/main/java/com/arwest/app/developer/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.arwest.app.developer.ppmtool.repositories;
+
+import com.arwest.app.developer.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
