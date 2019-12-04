eea073fad43b7d6a3930f8edfc479086b130dc64
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2019-12-03 20:59:39 -0600

    creating components Dashboard, Layout/header

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem'
+
 
 class Dashboard extends Component {
     render() {
         return (
+            <div>
             <h1>Welcome to the Dashboard</h1>
-        )
+            <ProjectItem/>
+            <ProjectItem/>
+            <ProjectItem/>
+            <ProjectItem/>
+            </div>
+        );
     }
 }
 export default Dashboard;
