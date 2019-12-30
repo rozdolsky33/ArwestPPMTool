bbab04ab12a93dc82dc90a0e2210d04a90dfce08
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2019-12-29 20:22:27 -0500

    adding props to dashboard to displey project items from the db

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -13,21 +13,27 @@ class Dashboard extends Component {
     }
 
     render() {
+        const {projects} = this.props.project
+       
         return (
-    <div className="projects">
-    <div className="container">
-        <div className="row">
-            <div className="col-md-12">
-                <h1 className="display-4 text-center">Projects</h1>
-                <br />
-                <CreateProjectButton/>
-                <br />
-                <hr />
-                <ProjectItem />
+        <div className="projects">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="display-4 text-center">Projects</h1>
+                        <br />
+                        <CreateProjectButton/>
+                        <br />
+                        <hr />
+                        {
+                        projects.map(project =>(
+                        <ProjectItem key={project.id}project={project} />
+                        ))
+                        }
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
         );
     }
 }
