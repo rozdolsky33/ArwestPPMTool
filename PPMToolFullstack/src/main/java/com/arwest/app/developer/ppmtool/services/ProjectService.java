020a2dc3f8d711a600831e195ae54dadd5c2a21b
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-02-20 02:03:10 -0500

    refactoring for update

--- PPMToolFullstack/src/main/java/com/arwest/app/developer/ppmtool/services/ProjectService.java
+++ PPMToolFullstack/src/main/java/com/arwest/app/developer/ppmtool/services/ProjectService.java
@@ -2,13 +2,54 @@ package com.arwest.app.developer.ppmtool.services;
 
 
 import com.arwest.app.developer.ppmtool.domain.Project;
-import com.arwest.app.developer.ppmtool.shared.dto.ProjectDto;
+import com.arwest.app.developer.ppmtool.exceptions.ProjectIdException;
+import com.arwest.app.developer.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface ProjectService {
+@Service
+public class ProjectService {
 
-    ProjectDto createProject(ProjectDto project);
-    ProjectDto getProjectById(String projectId);
-    Iterable<Project> getAllProjects();
-    ProjectDto updateProject(String projectId, ProjectDto projectDto);
-    void deleteProject(String projectId);
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+
+    }
+
+
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+
+        }
+
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+
+    public void deleteProjectByIdentifier(String projectid){
+        Project project = projectRepository.findByProjectIdentifier(projectid.toUpperCase());
+
+        if(project == null){
+            throw  new  ProjectIdException("Cannot Project with ID '"+projectid+"'. This project does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
+
 }
--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,32 +1,42 @@
-import axios from "axios";
-import {GET_ERRORS, GET_PROJECTS, GET_PROJECT} from "./types";
+import axios from 'axios';
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from './types';
 
 export const createProject = (project, history) => async dispatch => {
-    try {
-        const res = await axios.post("http://localhost:8080/api/project", project)
-        history.push("/dashboard")
-    } catch (error) {
-        dispatch({
-            type:GET_ERRORS,
-            payload:error.response.data
-        });
-        
-    }
+  try {
+    const res = await axios.post('http://localhost:8080/api/project', project);
+    history.push('/dashboard');
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
 };
 
 export const getProjects = () => async dispatch => {
-    const res = await axios.get("http://localhost:8080/api/project/all")
-    dispatch ({
-        type: GET_PROJECTS,
-        payload:res.data
-    });
+  const res = await axios.get('http://localhost:8080/api/project/all');
+  dispatch({
+    type: GET_PROJECTS,
+    payload: res.data
+  });
 };
 
 export const getProject = (id, history) => async dispatch => {
-
-    const res = await axios.get(`http://localhost:8080/api/project/${id}`)
+  const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: GET_PROJECT,
+    payload: res.data
+  });
+};
+export const updateProject = (id, project, history) => async dispatch => {
+  history.push('/dashboard');
+  try {
+    const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+    history.push('/dashboard');
+  } catch (error) {
     dispatch({
-        type: GET_PROJECT,
-        payload: res.data
+      type: GET_ERRORS,
+      payload: error.response.data
     });
+  }
 };
