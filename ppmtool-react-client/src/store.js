e331a51fac46bbca6597aa9c3807bd157c6076c3
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-02-27 07:13:15 -0500

    refactoring react

--- ppmtool-react-client/src/store.js
+++ ppmtool-react-client/src/store.js
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const initalState = {};
 const middleware = [thunk];
 
 let store;
 
-if (window.navigator.userAgent.includes("Chrome")) {
+if (window.navigator.userAgent.includes('Chrome')) {
   store = createStore(
     rootReducer,
     initalState,
